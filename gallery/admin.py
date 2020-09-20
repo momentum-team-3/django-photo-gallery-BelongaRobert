9334e33d994e15ff139be822e1368db1332d8b1a
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2020-09-20 10:42:45 -0400

    Adding Summary to Admin site

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from gallery.models import Album, Photo
+from gallery.models import Album, Photo, Summary
 
 # Register your models here.
 admin.site.register(Album)
 admin.site.register(Photo)
+admin.site.register(Summary)
